80b0211b4b8a40abbdc375d497f7957b8c6c10c5
xs9734 <42820350+xs9734@users.noreply.github.com>
2018-12-24 04:00:38 -0800

    navigation position fixes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 $(document).foundation()
+
+var x = document.getElementById('mobile-nav').offsetHeight;
+console.log(x);
+document.getElementById("topbar-center-logo").style.top = x +"px";
+
+window.onresize = function(){
+    var h = document.getElementById('mobile-nav').offsetHeight;
+    console.log(h);
+    document.getElementById("topbar-center-logo").style.top = h +"px";
+}
