39435c6e6765c75f839b07ab0eeab3b18cbef03b
xs9734 <42820350+xs9734@users.noreply.github.com>
2019-01-07 21:09:25 -0800

    initial contact commit

--- js/app.js
+++ js/app.js
@@ -3,9 +3,16 @@ $(document).foundation()
 var x = document.getElementById('mobile-nav').offsetHeight;
 console.log(x);
 document.getElementById("topbar-center-logo").style.top = x +"px";
+document.getElementById("first-box").style.paddingTop = x +"px";
 
 window.onresize = function(){
     var h = document.getElementById('mobile-nav').offsetHeight;
     console.log(h);
     document.getElementById("topbar-center-logo").style.top = h +"px";
+}
+
+window.onresize = function(){
+    var h = document.getElementById('mobile-nav').offsetHeight;
+    console.log(h);
+    document.getElementById("first-box").style.paddingTop = h +"px";
 }
